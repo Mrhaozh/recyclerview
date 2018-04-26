4ab47fc0c8782a6f7094eb1cab0afd8720ac6d6d
gittest <384813636@qq.com>
2018-04-26 18:28:20 +0800

    verson1

---
+++ app/src/main/java/com/example/debug/recyclerview/MyCardView.java
@@ -0,0 +1,34 @@
+package com.example.debug.recyclerview;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+
+public class MyCardView extends CardView{
+    public MyCardView(@NonNull Context context) {
+        super(context);
+    }
+
+    public MyCardView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyCardView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+       // super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getDefaultSize(0, widthMeasureSpec),
+                getDefaultSize(0, heightMeasureSpec));
+
+        int childWidthSize = getMeasuredWidth();
+        // 高度和宽度一样
+        heightMeasureSpec = widthMeasureSpec = MeasureSpec.makeMeasureSpec(
+                childWidthSize, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
--- app/src/main/java/com/example/debug/recyclerview/RecyclerAdapter.java
+++ app/src/main/java/com/example/debug/recyclerview/RecyclerAdapter.java
@@ -43,12 +43,12 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Recycl
             holder.imageView.setScaleType(ImageView.ScaleType.FIT_XY);
             Glide.with(context).load(urlList.get(position)).override(600, 600).into(holder.imageView);
         }else{
-            ViewGroup.LayoutParams params=holder.imageView.getLayoutParams();
-            params.height=240;
-            params.width=240;
-            holder.imageView.setLayoutParams(params);
+//            ViewGroup.LayoutParams params=holder.imageView.getLayoutParams();
+//            params.height=240;
+//            params.width=240;
+//            holder.imageView.setLayoutParams(params);
             holder.imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            Glide.with(context).load(urlList.get(position)).override(300, 300).into(holder.imageView);
+            Glide.with(context).load(urlList.get(position)).override(200, 200).into(holder.imageView);
         }
     }
 
